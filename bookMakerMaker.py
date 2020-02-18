61db04bc25d93da3b8624b09cdaafba354590e54
RossignolD <meirconnor@gmail.com>
2020-02-18 18:09:30 -0500

    oy vey

--- bookMakerMaker.py
+++ bookMakerMaker.py
@@ -7,10 +7,12 @@ import template as t
 import json
 import copy
 import shutil
+import getpass
 os.chdir(sys.path[0])
-BookStem="PHY132_Book_"
-instructor_netid="themmick"
-dev_netid="evannieuwenh"
+BookStem="TeleHealth_"
+instructor_netid="mdtmp"
+dev_netid=getpass.getuser()
+startingLectureDescriptionIndex=1
 ##title='.'.join(sys.argv[1].split('/')[1][::-1].split(".")[1:])[::-1]
 def fitem(item):
     item=item.strip()
@@ -26,13 +28,19 @@ with open(sys.argv[1], 'r') as csvin:
 
 bookIndices= set()
 for row in data:
-    bookIndices.add(row["Book"])
+    if row["Book"].strip()=="":
+        continue
+    bookIndices.add(int(row["Book"]))
+
+print sorted(bookIndices)
 
 
-for i in bookIndices:
+for idx in bookIndices:
     template=copy.deepcopy(t.template)
-    localStorageKey=BookStem+i
-    currentBook=filter(lambda x: x["Book"]==i,data)
+    localStorageKey=BookStem+str(idx)
+    currentBook=filter(lambda x: x["Book"]==str(idx),data)
+    print currentBook
+
     template["chapters"][0]["title"]=localStorageKey.replace("_"," ")
 
     pages = {}
@@ -52,18 +60,19 @@ for i in bookIndices:
 
     for i in sorted (pages):
         templateChapter=copy.deepcopy(template["chapters"][0])
-        templateChapter['content']="localData/TitlePdf/Lecture%d-descriptions.html"%(i)
+        templateChapter['content']="localData/TitlePdf/Lecture%d-descriptions.html"%(startingLectureDescriptionIndex)
         templateChapter["pages"]=[]
         for j in pages[i]:
             templateChapter["pages"].append(j)
         template["chapters"].append(templateChapter)
+        startingLectureDescriptionIndex+=1
     template["chapters"]=template["chapters"][1:]
     module= json.dumps(template, sort_keys=True,indent=4, separators=(',', ': '))
 
-    src="/Users/%s/Desktop/Apps/bookMaker/BookTemplate" %(dev_netid)
+    src="/Users/%s/Desktop/Apps/bookMaker/Users/dummyUser/Template" %(dev_netid)
     dst="/Users/%s/Desktop/Apps/bookMaker/Users/%s/%s" %(dev_netid,instructor_netid,localStorageKey)
-    os.system("cp -r '%s' '%s'" %(src,dst))
-    #shutil.copy(src,dst,follow_symlinks=False)
+    #os.system("cp - '%s' '%s'" %(src,dst))
+    shutil.copytree(src,dst,symlinks=True)
     f = open("%s/module.json"%(dst), "w")
     f.write(module)
 #
